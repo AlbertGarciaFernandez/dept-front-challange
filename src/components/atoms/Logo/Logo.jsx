ec53d2ab12532dc142013743751aab7e6296ede7
Albert Garcia <alberd_beto@hotmail.com>
2022-07-21 15:57:17 +0200

    logo dept add

---
+++ src/components/atoms/Logo/Logo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DeptLogoWhite from "../../../assets/svg/DeptLogoWhite.svg";
+
+function Logo() {
+  return (
+    <div className="logoWrapper">
+      <img className="hamburgerImgLogo" src={DeptLogoWhite} alt="logo" />
+    </div>
+  );
+}
+
+export default Logo;
