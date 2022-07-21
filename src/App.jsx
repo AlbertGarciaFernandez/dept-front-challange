d01a648e783d2f550f9ddbf196ee2c12be4da324
Albert Garcia <alberd_beto@hotmail.com>
2022-07-21 13:13:17 +0200

    sass config installed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Desktop from "./pages/Desktop/Desktop";
+
+function App() {
+  return (
+    <div className="App">
+      <Desktop />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Desktop/Desktop.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "../../components/organism/NavBar/Navbar";
+function Desktop() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default Desktop;
