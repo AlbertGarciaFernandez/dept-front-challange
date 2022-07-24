238ac59b8b13ddcd393e809af215c84e7c416179
Albert Garcia <alberd_beto@hotmail.com>
2022-07-24 20:24:54 +0200

    input added

---
+++ src/components/molecules/Input/Input.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./Input.scss";
+
+import { Field } from "formik";
+
+function Input({
+  label = "field-1",
+  name = "field-1",
+  error,
+  touched,
+  placeholder = "",
+}) {
+  return (
+    <div className="inputContainer">
+      <div className="inputWrapper">
+        <label htmlFor={`${name}Input`}>
+          {label}
+          <Field
+            name={name}
+            id={`${name}Input`}
+            placeholder={placeholder}
+            className={error && touched ? "inputError" : ""}
+          />
+        </label>
+      </div>
+      <InputError error={error} touched={touched} />
+    </div>
+  );
+}
+
+export function InputError({ error, touched, size = "1em" }) {
+  const styles = {
+    fontSize: size,
+  };
+  return (
+    <div style={styles} className="errorText">
+      {error && touched ? `${error}` : ""}
+    </div>
+  );
+}
+
+export default Input;
