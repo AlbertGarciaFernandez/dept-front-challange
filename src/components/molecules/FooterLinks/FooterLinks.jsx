dcf0fb74f98d03e8e2b72d8def1201fff5f43fc7
Albert Garcia <alberd_beto@hotmail.com>
2022-07-24 20:26:05 +0200

    footer links added

---
+++ src/components/molecules/FooterLinks/FooterLinks.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./FooterLinks.scss";
+
+function FooterLinks() {
+  return (
+    <div className="footerLinksWrapper">
+      <ul className="footerLinksList">
+        <li>Work</li>
+        <li>Services</li>
+        <li>Stories</li>
+        <li>About</li>
+        <li>Careers</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+}
+
+export default FooterLinks;
