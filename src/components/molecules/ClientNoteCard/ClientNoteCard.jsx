4cdf83a4b5a2c4709b450c6ed96442194430a725
Albert Garcia <alberd_beto@hotmail.com>
2022-07-24 20:29:03 +0200

    client note card added

---
+++ src/components/molecules/ClientNoteCard/ClientNoteCard.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./ClientNoteCard.scss";
+import ReadMore from "../ReadMore/ReadMore";
+
+function ClientNoteCard(props) {
+  return (
+    <div className="ClientNoteCard">
+      <img
+        src={props.img}
+        className="ClientNoteCardImage"
+        alt="ClientNoteCardBackground"
+      />
+      <div className="ClientNoteCardBottomContainer">
+        <p className="ClientNoteCardTitle">{props.title}</p>
+        <p className="ClientNoteCardText">{props.text}</p>
+        <ReadMore />
+      </div>
+    </div>
+  );
+}
+
+export default ClientNoteCard;
