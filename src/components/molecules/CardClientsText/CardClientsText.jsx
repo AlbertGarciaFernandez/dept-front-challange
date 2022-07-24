9eeb0cfd437e7abe0c2d80c1b4b2c28ca2626aef
Albert Garcia <alberd_beto@hotmail.com>
2022-07-24 20:25:16 +0200

    cardclients text added

---
+++ src/components/molecules/CardClientsText/CardClientsText.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./CardClientsText.scss";
+
+import Note from "../Note/Note";
+
+function CardClientsText() {
+  return (
+    <section className="cardClientTextSection">
+      <div className="cardClientWrapper">
+        <Note
+          header="MICROSOFT"
+          text="Tapping into Ireland’s unique gaming culture and bringing a fresh flavour to their Xbox social media channels"
+        />
+        <Note
+          header="O’NEILL"
+          text="Integrating existing content into O’Neills’s new e-commerce platform"
+        />
+        <Note
+          header="MICROSOFT"
+          text="Tapping into Ireland’s unique gaming culture and bringing a fresh flavour to their Xbox social media channels"
+        />
+      </div>
+    </section>
+  );
+}
+
+export default CardClientsText;
