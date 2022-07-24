a93167aa028b2b4ab363b159b531ca99545ace6c
Albert Garcia <alberd_beto@hotmail.com>
2022-07-24 20:24:28 +0200

    button send

---
+++ src/components/atoms/ButtonSend/ButtonSend.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./ButtonSend.scss";
+
+function Button() {
+  return (
+    <div className="buttonWrapper">
+      <button type="submit" className="buttonSend">
+        Send
+      </button>
+    </div>
+  );
+}
+
+export default Button;
