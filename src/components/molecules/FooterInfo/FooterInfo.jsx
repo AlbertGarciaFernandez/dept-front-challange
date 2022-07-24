8bb8170418240acab6c8d3dfe32341bd3463867a
Albert Garcia <alberd_beto@hotmail.com>
2022-07-24 20:28:05 +0200

    footer info added

---
+++ src/components/molecules/FooterInfo/FooterInfo.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./FooterInfo.scss";
+
+function FooterInfo() {
+  return (
+    <section className="footerInfoSection">
+      <div className="footerInfoWrapper">
+        <ul className="footerInfoList">
+          <li>Chamber of Commerce: 63464101 </li>
+          <li>VAT: NL 8552.47.502.B01</li>
+          <li>Terms and conditions</li>
+        </ul>
+      </div>
+      <div className="footerDeptAgency©">© 2022 Dept Agency</div>
+    </section>
+  );
+}
+
+export default FooterInfo;
