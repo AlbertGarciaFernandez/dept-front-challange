17c9a5e17d9fc9c9276ff000f8e9493f2f497026
Albert Garcia <alberd_beto@hotmail.com>
2022-07-24 20:28:34 +0200

    clientquote component added

---
+++ src/components/organism/ClientQuote/ClientQuote.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./ClientQuote.scss";
+
+function ClientQuote() {
+  return (
+    <div className="clientQuoteWrapper">
+      <p className="clientQuoteText">
+        “Dept helped us tell our story through a bold new identity and a robust
+        online experience. To the tone of 60% growth in online bookings in just
+        one month”
+      </p>
+      <p className="clientQuoteAutor">MATTIJS TEN DRINK - CEO, TRANSAVIA</p>
+    </div>
+  );
+}
+
+export default ClientQuote;
