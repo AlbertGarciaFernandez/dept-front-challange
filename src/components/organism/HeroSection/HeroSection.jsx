7ad02d7a5c252269bccc725132e36213c10fa010
Albert Garcia <alberd_beto@hotmail.com>
2022-07-24 20:27:21 +0200

    hero section added

---
+++ src/components/organism/HeroSection/HeroSection.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./HeroSection.scss";
+
+function HeroSection() {
+  return (
+    <div className="heroSectionWrapper">
+      <p className="heroSectionTitle">Work</p>
+      <p className="heroSectionText">
+        A selection of projects that <b>pioneer tech</b> and<b> marketing</b> to
+        help brands stay ahead.
+      </p>
+    </div>
+  );
+}
+
+export default HeroSection;
