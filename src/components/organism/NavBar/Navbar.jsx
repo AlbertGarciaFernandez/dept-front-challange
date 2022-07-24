d72b8635869569c48a07ea03c4105416308a498a
Albert Garcia <alberd_beto@hotmail.com>
2022-07-24 20:26:26 +0200

    navbar added

--- src/components/organism/NavBar/Navbar.jsx
+++ src/components/organism/NavBar/Navbar.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import "./Navbar.scss";
 import Logo from "../../atoms/Logo/Logo";
+
 import PrimaryNavigation from "../../molecules/PrimaryNavigation/PrimaryNavigation";
+import DotsModal from "../../molecules/DotsModal/DotsModal";
 
 function Navbar() {
   return (
     <div className="navbarContainer">
       <header className="headerWrapper">
         <Logo />
-
         <PrimaryNavigation />
+        <DotsModal />
       </header>
     </div>
   );
