01164826d31fe72d84575be6c94cff351c1d9741
Albert Garcia <alberd_beto@hotmail.com>
2022-08-03 00:40:17 +0200

    atoms mobile responsive

--- src/components/atoms/Logo/Logo.jsx
+++ src/components/atoms/Logo/Logo.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import "./Logo.scss";
 import DeptLogoWhite from "../../../assets/svg/DeptLogoWhite.svg";
 
 function Logo() {
   return (
     <div className="logoWrapper">
-      <img className="imgLogo" src={DeptLogoWhite} alt="logo" />
+      <img
+        className="imgLogo"
+        src={DeptLogoWhite}
+        alt="logo"
+        data-testid="logo-1"
+      />
     </div>
   );
 }
