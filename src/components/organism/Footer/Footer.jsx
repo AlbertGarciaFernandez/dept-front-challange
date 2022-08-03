58a1ba7a5fe8552136bf9044551178eba353ab71
Albert Garcia <alberd_beto@hotmail.com>
2022-08-03 03:47:59 +0200

    Footers icons in mobile added

--- src/components/organism/Footer/Footer.jsx
+++ src/components/organism/Footer/Footer.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+
+//components
 import Arrow from "../../atoms/Arrow/Arrow";
 import FooterInfo from "../../molecules/FooterInfo/FooterInfo";
 import FooterLinks from "../../molecules/FooterLinks/FooterLinks";
 import Logo from "../../atoms/Logo/Logo";
+
+//assets
+import Facebook from "../../../assets/svg/Facebook.svg";
+import Twitter from "../../../assets/svg/Twitter.svg";
+import Instagram from "../../../assets/img/instagram.png";
 
 import "./Footer.scss";
 
@@ -11,8 +18,19 @@ function Footer() {
     <section className="footerSection">
       <div className="footerBlackWrapper">
         <div className="footerUpSection">
-          <Logo />
+          <div className="footerLogoWrapper">
+            <Logo />
+          </div>
           <FooterLinks />
+          <div className="iconSocialNetworks">
+            <img className="FacebookIcon" src={Facebook} alt="FacebookIcon" />
+            <img className="TwitterIcon" src={Twitter} alt="TwitterIcon" />
+            <img
+              className="InstagramIcon"
+              src={Instagram}
+              alt="InstagramIcon"
+            />
+          </div>
         </div>
         <div className="footerDownSection">
           <FooterInfo />
